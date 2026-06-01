509030b927b0870b0bd48f0216fa60eb7ad40b07
DhruvkrSharma <dhruv258.kumar@gmail.com>
2026-06-01 19:57:31 +0530

    fix: eslint config - ignore .next and cloudflare-worker dirs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "cloudflare-worker/**",
+      "node_modules/**",
+    ],
+  },
 ];
 
 export default eslintConfig;
